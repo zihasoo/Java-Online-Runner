08e32b869b0718280636cee93e5c709eda57d782
zihasoo <dlquddbs-@daum.net>
2022-11-05 23:04:06 +0900

    테마 변경 심스의 도움을 받아 개선

--- src/Problems/index.js
+++ src/Problems/index.js
@@ -1,4 +1,3 @@
-
 let result = document.getElementById("result");
 let dropdown = document.getElementById("dropdown");
 let editor = ace.edit(document.getElementById("code_area"), {
@@ -7,20 +6,39 @@ let editor = ace.edit(document.getElementById("code_area"), {
 });
 let input_box = document.getElementById("input_box");
 
-function change_theme(){
+let colors = {
+    cobalt: "#052440",
+    dracula: "#303440",
+    dreamweaver: "#ffffff",
+    one_dark: "#282C34",
+    monokai: "#272822",
+    github: "#ffffff",
+};
+
+function change_theme() {
+    let color = colors[dropdown.value];
+    let char_color;
+
+    if(color === "#ffffff") char_color = "#000000";
+    else char_color = "#ffffff";
+
+    input_box.style.color = char_color;
+    result.style.color = char_color;
+    input_box.style.backgroundColor = color;
+    output_box.style.backgroundColor = color;
+
     editor.setTheme("ace/theme/" + dropdown.value);
 }
 
-function compile_code () {
+function compile_code() {
     let url = "https://runjava.mcv.kr/runCode";
     let body;
     result.textContent = "실행 중 . . .";
 
-    if(input_box.value !== ""){
+    if (input_box.value !== "") {
         url += "WithInput"
-        body = JSON.stringify({"code":editor.getValue(),"input":input_box.value})
-    }
-    else {
+        body = JSON.stringify({"code": editor.getValue(), "input": input_box.value})
+    } else {
         body = editor.getValue();
     }
 
